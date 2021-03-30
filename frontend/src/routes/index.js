d17793e1ad6c3793a792d85c4c2a149471b12725
zequi <calvo.ezequias@gmail.com>
2021-03-30 04:40:29 -0300

    change names SignIn SignUp

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import history from "../history";
 import Home from "../components/home";
-import Login from "../components/Login";
 import CreateRestaurant from "../components/profile/restaurants/CreateRestaurant";
+// test. delete this
 
 function OurRouter() {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/sign-in/" exact component={Login}/>
         <Route path="/create-restaurant/" exact component={CreateRestaurant} />
         <Route path="*" component={() => "404 NOT FOUND"} />
       </Switch>
