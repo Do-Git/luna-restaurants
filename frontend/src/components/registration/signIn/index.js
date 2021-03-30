ddd393afe1da6dcae3e1c7744020fe78d88b8020
Domenico Dongiovanni <domenicodongiovanni@Domenicos-iMac.local>
2021-03-30 06:32:15 +0200

    registration pages

--- frontend/src/components/registration/signIn/index.js
+++ frontend/src/components/registration/signIn/index.js
@@ -1,7 +1,7 @@
 //import { connect } from 'react-redux';
 import React from 'react';
 // import SignUpLink from "./SignUpLink";
-import SignInBody from "./signInBody";
+import SignInBody from "./SignInBody";
 
 
 export const SignIn = () => {
