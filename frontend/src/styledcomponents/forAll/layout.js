69b0e3d68ba1bbf6534b8e44aea97166bab2997f
Kanoknuch Klopfenstein <eve.klopfenstein@gmail.com>
2021-03-31 10:32:56 +0200

    fix conflict on forAll/layout

--- frontend/src/styledcomponents/forAll/layout.js
+++ frontend/src/styledcomponents/forAll/layout.js
@@ -6,26 +6,28 @@ export const MiddleSection = styled.div`
   flex-direction: column;
   align-items: center;
   background-color: #f2f2f2;
-`;
+`
 
 export const H1 = styled.h1`
   font-size: 2rem;
   margin-top: 100px;
-`;
+`
+
 export const OrangeDiv = styled.div`
   width: 100px;
   border: 3px solid #e47d31;
-`;
+`
+
 export const TopDiv = styled.div`
   margin-top: 50px;
-`;
+`
+
 export const BottomDiv = styled.div`
     margin-top:50px
     flex-direction: column;
     align-items: flex-start;
-`;
+`
 
-<<<<<<< HEAD
 export const FlexDiv = styled.div`
     display: flex;
 `
@@ -43,15 +45,3 @@ export const FlexSpaceAroundDiv = styled(FlexDiv)`
     justify-content: space-around;
     align-items: center;
 `
-=======
-export const FlexCenterDiv = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-export const FlexSpaceBetweenDiv = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 1rem;
-`;
->>>>>>> dev
