9b1e6fb1daf84a38f82287159a9ed0de498d0bed
Domenico Dongiovanni <domenicodongiovanni@Domenicos-iMac.local>
2021-04-01 11:04:53 +0200

    create review update

--- frontend/src/components/search/restaurants/restaurantPageWriteReview/RestaurantPageReviews/CreateReview/index.jsx
+++ frontend/src/components/search/restaurants/restaurantPageWriteReview/RestaurantPageReviews/CreateReview/index.jsx
@@ -19,11 +19,42 @@ import {FlexSpaceBetweenDiv} from "../../../../../../styledcomponents/forAll/lay
 
 const CreateReview = () => {
 
-    const [rating, setRating] = useState(0);
-    const [showComments, setShowComments] = useState(false);
+    const token = localStorage.getItem('token');
+    const [review, setReview] = useState('');
+    const [rating, setRating] = useState('');
+    
+
+    const Review = e => {
+        e.preventDefault();
+        const url = "https://luna-sagittarius.propulsion-learn.ch/backend/api/reviews/new/";
+        const method = 'POST';
+        const body = {
+            review: review,
+            rating: rating,
+        };
+        const headers = new Headers({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        });
+        const config = {
+            method: method,
+            headers: headers,
+            body: JSON.stringify(body)
+        };
+        fetch(url, config)
+        .then(res => res.status)
+        .then(status => {
+            if (status === 201){
+                console.log("ok")
+            } else {
+                console.log("response not ok");
+            }
+        });
+    }
 
     return(
             <ReviewCardCenter>  
+                <form onSubmit={Review}>
                     <ReviewCardFirst> 
                         <StarsReviewRestaurantPageContainer id='stars-container-review'>
                             <StarRatingComponent
@@ -41,7 +72,9 @@ const CreateReview = () => {
                     </ReviewCardSecond>
                         <ReviewCardSecond>
                         </ReviewCardSecond>
-                    <ReviewCardSecond><p>This field is required</p><OrangeSmallSubmitButton >SUBMIT</OrangeSmallSubmitButton></ReviewCardSecond>
+                    <ReviewCardSecond><p>This field is required</p> 
+                    <OrangeSmallSubmitButton id='review' value={review} onChange={event => setReview(event.target.value)} required>SUBMIT</OrangeSmallSubmitButton></ReviewCardSecond>
+                </form>      
             </ReviewCardCenter>
     )
 }
