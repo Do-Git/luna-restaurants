5fcace35136b3615fa72ea128be579699e808d97
David Brown <davidbr996@gmail.com>
2021-04-01 09:55:44 +0200

    new changes

--- frontend/src/components/profile/userProfile/About/index.jsx
+++ frontend/src/components/profile/userProfile/About/index.jsx
@@ -3,19 +3,19 @@ import { useSelector } from 'react-redux';
 
 const About = () => {
 
-    const dateJoined = useSelector(state => state.date_joined);
-    const location = useSelector(state => state.location);
-    const Description = useSelector(state => state.description);
-    const thingsILove = useSelector(state => state.things_I_love);
-    const firstName = useSelector(state => state.first_name);
-    const lastName = useSelector(state => state.last_name);
-    const phone = useSelector(state => state.phone);
+    const dateJoined = useSelector(state => state.mixReducers.date_joined);
+    const location = useSelector(state => state.mixReducers.location);
+    const Description = useSelector(state => state.mixReducers.description);
+    const thingsILove = useSelector(state => state.mixReducers.things_I_love);
+    const firstName = useSelector(state => state.mixReducers.first_name);
+    const lastName = useSelector(state => state.mixReducers.last_name);
+    const phone = useSelector(state => state.mixReducers.phone);
     
-    const monthNum = dateJoined ? parseInt(dateJoined.slice(4, 6)) : 0;
+    const monthNum = dateJoined ? parseInt(dateJoined.slice(4, 6)) : -1;
     const day = dateJoined ? parseInt(dateJoined.slice(6, 8)) : 0;
     const year = dateJoined ? parseInt(dateJoined.slice(0, 4)) : 0;
 
-    const date = monthNum ? new Date(year, day, monthNum) : 0;
+    const date = monthNum !== -1 ? new Date(year, day, monthNum) : 0;
     const month = date.toLocaleString('default', {month: 'long'});
 
 
@@ -33,7 +33,7 @@ const About = () => {
             <h3>Things I love</h3>
             <p>{ thingsILove }</p>
             <h3>Description</h3>
-            <p>{ Description }</p>
+            <p id="description" >{ Description }</p>
         </AboutContainer>
     )
 }
