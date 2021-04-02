2c6bdeb03e54021f04c27dda97c08ec7c3c92600
Dusko <radovic_dusko@yahoo.com>
2021-04-02 11:05:03 +0200

    new endpoint

--- backend/comment/urls.py
+++ backend/comment/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from comment.views import CreateNewComment, GetCommentsByUser, DeleteComment, ToggleLikeComment
+from comment.views import CreateNewComment, GetCommentsByUser, DeleteComment, ToggleLikeComment, GetCommentsByReview
 
 urlpatterns = [
     path('new/<int:pk>/', CreateNewComment.as_view()),
     path('<int:user_id>/', GetCommentsByUser.as_view()),
     path('delete/<int:comment_id>/', DeleteComment.as_view()),
-    path('like/<int:pk>/', ToggleLikeComment.as_view())
+    path('like/<int:pk>/', ToggleLikeComment.as_view()).
+    path('review/<review_id>/', GetCommentsByReview.as_view())
     ]
