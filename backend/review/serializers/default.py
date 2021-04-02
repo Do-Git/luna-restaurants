5296072ffbda50a4d1da0aed4df388d029cbde63
Dusko <radovic_dusko@yahoo.com>
2021-04-02 14:18:15 +0200

    few tweaks to endpoints

--- backend/review/serializers/default.py
+++ backend/review/serializers/default.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from comment.serializers.default import CommentBasicSerializer
 from restaurant.serializers.nested import RestaurantInUserSerializer
 from review.models import Review
 from user.serializers.nested import UserInReviewSerializer
@@ -9,6 +10,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     author = UserInReviewSerializer(read_only=True)
     restaurant = RestaurantInUserSerializer(read_only=True)
     liked_by = UserInReviewSerializer(read_only=True, many=True)
+    comments = CommentBasicSerializer(read_only=True, many=True)
 
     class Meta:
         model = Review
